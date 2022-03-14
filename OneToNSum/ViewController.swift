a6831b9d3e583b57f7418ddeea206a33bfab4bae
cihancalli <cihan.callii@gmail.com>
2022-03-14 23:15:59 +0300

    Sum of numbers from 1 to n entered

--- OneToNSum/ViewController.swift
+++ OneToNSum/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var TitleLabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
+    @IBOutlet weak var NTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func didTabSumButton(_ sender: Any) {
+        
+        let nInt : Int = Int(NTextField.text!) ?? 0
+        var nSum : Int = 0
 
+        nSum = ( nInt * (nInt + 1) / 2)
+        
+        TotalLabel.text = "\(nSum)"
+        
+        TitleLabel.text = "1'den \(nInt)'e kadar sayıların toplamı"
+        
+    }
+    
 }
 
